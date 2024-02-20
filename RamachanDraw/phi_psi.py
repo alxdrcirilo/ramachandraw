2634f652b89105c4cd5605fe2017ee6406446ce0
Fabian Tann <fabian.tann@mni.thm.de>
2024-02-20 12:34:40 +0100

    outsourced function and added quiet mode, added parameters, code polishing

--- RamachanDraw/phi_psi.py
+++ RamachanDraw/phi_psi.py
@@ -1,36 +1,47 @@
-from Bio.PDB import PDBParser, PPBuilder
 from math import pi
+
+from Bio.PDB import PDBParser, PPBuilder
 from rich.console import Console
 from rich.table import Table
 
 console = Console(color_system='windows')
 
 
-def phi_psi(pdb_file, return_ignored=False):
-    def get_ignored_res(file: str):
-        x, y, ignored, output = [], [], [], {}
-        for model in PDBParser().get_structure(id=None, file=file):
-            for chain in model:
-                peptides = PPBuilder().build_peptides(chain)
-                for peptide in peptides:
-                    for aa, angles in zip(peptide, peptide.get_phi_psi_list()):
-                        residue = chain.id + ":" + aa.resname + str(aa.id[1])
-                        output[residue] = angles
+def get_ignored_res(pdb_file_path: str, ignore_pdb_warnings: bool = False) -> (dict, list, list, list):
+    x, y, res_ignored, res_output = [], [], [], {}
+    for model in PDBParser(PERMISSIVE=False, QUIET=ignore_pdb_warnings).get_structure(id=None, file=pdb_file_path):
+        for chain in model:
+            peptides = PPBuilder().build_peptides(chain)
+            for peptide in peptides:
+                for aa, angles in zip(peptide, peptide.get_phi_psi_list()):
+                    residue = chain.id + ":" + aa.resname + str(aa.id[1])
+                    res_output[residue] = angles
 
-        for key, value in output.items():
-            # Only get residues with both phi and psi angles
-            if value[0] and value[1]:
-                x.append(value[0] * 180 / pi)
-                y.append(value[1] * 180 / pi)
-            else:
-                ignored.append((key, value))
+    for key, value in res_output.items():
+        # Only get residues with both phi and psi angles
+        if value[0] and value[1]:
+            x.append(value[0] * 180 / pi)
+            y.append(value[1] * 180 / pi)
+        else:
+            res_ignored.append((key, value))
 
-        return output, ignored, x, y
+    return res_output, res_ignored, x, y
+
+
+def phi_psi(pdb_file, return_ignored: bool = False, print_ignored: bool = False, ignore_pdb_warnings: bool = False):
+    if ignore_pdb_warnings:
+        try:
+            from Bio.PDB.PDBExceptions import PDBConstructionWarning
+            from warnings import simplefilter
+            simplefilter('ignore', PDBConstructionWarning)
+        except ImportError:
+            pass
 
     def start(fp: str):
-        phi_psi_data, ignored_res, x, y = get_ignored_res(file=fp)
+        phi_psi_data, ignored_res, x, y = get_ignored_res(pdb_file_path=fp, ignore_pdb_warnings=ignore_pdb_warnings)
 
-        if return_ignored:
+        # print ignored residue table
+        if print_ignored:
             table = Table(title='Ignored residues')
             table.add_column('Aminoacid\nresidue', style='red')
             table.add_column('\u03C6-\u03C8\nangles', justify='center')
@@ -38,14 +49,15 @@ def phi_psi(pdb_file, return_ignored=False):
                 table.add_row(_[0], str(_[1]))
             console.print(table)
 
+        if return_ignored:
             return phi_psi_data, ignored_res
         else:
             return phi_psi_data
 
-    if type(pdb_file) is str:
+    output = []
+    if isinstance(pdb_file, str):
         output = start(fp=pdb_file)
-    if type(pdb_file) is list:
-        output = []
+    elif isinstance(pdb_file, list):
         for file in pdb_file:
             output.append(start(fp=file))
 
